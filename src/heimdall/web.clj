9f51ae70816e6ee3fe40beaf075c9855e090149e
mcastanheira <mcastanheira@globo.com>
2019-03-02 14:27:42 -0300

    added filter to selmer to format timestamps

--- src/heimdall/web.clj
+++ src/heimdall/web.clj
@@ -6,7 +6,8 @@
     [ring.adapter.jetty :as jetty]
     [ring.middleware.params :refer :all]
     [ring.util.response :as response]
-    [selmer.parser :as selmer])
+    [selmer.parser :as selmer]
+    [selmer.filters :as filters])
   (:import [java.text SimpleDateFormat]))
 
 (selmer/set-resource-path! (clojure.java.io/resource "./templates"))
@@ -68,24 +69,32 @@
 ;      [:tbody (map #(check-row % timestamp-mask show-links) checks)]]
 ;      (if show-links [:script {:src "/js/reload.js"}])))
 
+(defn- load-checks-page [checks timestamp-mask show-links]
+  (selmer/render-file 
+    "checks.html" 
+    {:checks checks :df (SimpleDateFormat. timestamp-mask) :show-links show-links}))
+
 ;(defn- not-found-page []
 ;  (hiccup/html [:div {:class "alert alert-danger"} "Page not found!"]))
 
 (defn start-server 
   "Start the web server" 
   [config]
-  (let [{:keys [port check-interval timestamp-mask]} config]
+  (let [
+    {:keys [port check-interval timestamp-mask]} config 
+    date-format (SimpleDateFormat. timestamp-mask)]
+    (filters/add-filter! :format-timestamp (fn [timestamp] (.format date-format (java.util.Date. timestamp))))
     (jetty/run-jetty 
       (wrap-params
         (routes 
-;          (GET "/" [] (response/redirect "/checks"))
+          (GET "/" [] (response/redirect "/checks"))
           (GET "/configurations" [] (load-configurations-page port check-interval timestamp-mask))
           (GET "/services" [] (load-services-page (database/get-services) timestamp-mask))
           (POST "/services" request  (save-service (transform-form-params-to-service (:form-params request))))
           (GET "/services/add" [] (load-add-service-page))
           (GET "/services/edit/:id" [id] (load-edit-service-page (database/get-service (Integer/parseInt id))))
           (POST "/services/delete" request (delete-service (Integer/parseInt (get (:form-params request) "id"))))
-;          (GET "/checks" [] (generate-page (checks-page (database/get-last-checks) timestamp-mask true)))
-;          (GET "/checks/:id" [id] (generate-page (checks-page (database/get-last-checks-by-service (Integer/parseInt id) 10) timestamp-mask false)))
+          (GET "/checks" [] (load-checks-page (database/get-last-checks) timestamp-mask true))
+          (GET "/checks/:id" [id] (load-checks-page (database/get-last-checks-by-service (Integer/parseInt id) 10) timestamp-mask false))
           (route/resources "/")));          (route/not-found (generate-page (not-found-page)))))
       {:port (:port config) :join? false})))
